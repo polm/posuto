eab07e0ab19c85bfabd269a031566fe8bbb98c8f
Paul O'Leary McCann <polm@dampfkraft.com>
2019-12-03 18:01:33 +0900

    Version bump

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def gzip_postal_data():
 
 setup(
     name='posuto',
-    version='0.1.2',
+    version='0.1.3',
     url='https://github.com/polm/posuto.git',
     author="Paul O'Leary McCann",
     author_email='polm@dampfkraft.com',
