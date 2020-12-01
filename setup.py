2f4e18634246debbbc1bc12814fbe26a099cc300
Paul O'Leary McCann <polm@dampfkraft.com>
2020-12-01 15:28:13 +0900

    Remove json compression from setup.py

--- setup.py
+++ setup.py
@@ -4,25 +4,6 @@ import gzip
 import shutil
 from pathlib import Path
 
-def gzip_postal_data():
-    # remove the existing file
-    curdir = Path(__file__).parent
-    jsonfile = curdir / 'posuto/postaldata.json'
-    gzfile = curdir / 'posuto/postaldata.json.gz'
-
-    if gzfile.is_file() and not jsonfile.is_file():
-        # this is an install that already has the gzip
-        return
-
-    if gzfile.is_file() and gzfile.stat().st_mtime > jsonfile.stat().st_mtime:
-        # we're up to date, nothing to do.
-        return
-
-    # zip the non-compressed file
-    with open(jsonfile, 'rb') as f_in:
-        with gzip.open(gzfile, 'wb') as f_out:
-            shutil.copyfileobj(f_in, f_out)
-
 setup(
     name='posuto',
     use_scm_version=True,
@@ -36,5 +17,3 @@ setup(
     setup_requires=['setuptools-scm'],
 )
 
-if __name__ == '__main__':
-    gzip_postal_data()
